4dbea786798f33353959f1966f84a22f8739893d
shishirsabbir <shishir.sabbir@gmail.com>
2023-08-27 14:41:43 +0600

    svelte loop

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,45 @@
 <script>
-	let input_field = "Orange";
+	let firstName = "Shishir";
+	let lastName = "Sabbir";
+	// let beltColor = "Orange";
+
+	// let fullName = firstName + ' ' + lastName;
+	// reactive value
+	$: fullName = `${firstName} ${lastName}`;
+
+	// reactive statements
+	$: {
+		console.log(firstName);
+		// console.log(beltColor);
+	}
+
+	// people array
+	let people = [
+		{name: 'Yoshi', beltColor: 'Black', age: 25, id: 1},
+		{name: 'Mario', beltColor: 'Blue', age: 23, id: 2},
+		{name: 'Luigi', beltColor: 'Orange', age: 21, id: 3},
+		{name: 'Link', beltColor: 'Red', age: 26, id: 4},
+		{name: 'Chun-Li', beltColor: 'Green', age: 19, id: 5}
+	];
 	
 	const handleInput = e => {
-		input_field = e.target.value;
+		beltColor = e.target.value;
 	}
 </script>
 
 <main>
-	<h1>Type Anything</h1>
-	<p style="color: {input_field};">{input_field}</p>
-	<input type="text" bind:value={input_field}>
+	<h1>People Card</h1>
+	<div class="container">
+		{#each people as person}
+			<div class="box" style="border: 1px solid {person.beltColor};">
+				<h3>{person.name}</h3>
+				<p class="age">{person.age} years old</p>
+				<p class="belt"><span style="color: {person.beltColor};">{person.beltColor}</span>-Belt Ninja</p>
+			</div>
+		{:else}
+			<p>No People to Show</p>
+		{/each}
+	</div>
 </main>
 
 <style>
@@ -27,9 +57,34 @@
 		font-weight: 100;
 	}
 
-	p {
+	div.container {
+		display: flex;
+		align-items: center;
+		justify-items: center;
+		flex-wrap: wrap;
+		flex-direction: column;
+
+		/* width: 600px; */
+	}
+
+	p.belt {
 		font-variant: small-caps;
 		font-size: 1.2em;
+		text-transform: capitalize;
+	}
+
+	p.age {
+		/* font-variant: small-caps; */
+		font-size: 1em;
+		text-transform: capitalize;
+	}
+
+	div.box {
+		width: 400px;
+		height: 125px;
+		/* border: 1px solid #9f9f9f; */
+		margin: 10px;
+		flex-shrink: 0;
 	}
 
 	@media (min-width: 640px) {
